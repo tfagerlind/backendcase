71163926989374a26825d34f09bf19af3ffc1e68
Tomas Fägerlind <tomas.fagerlind@sofengi.com>
2023-08-24 13:26:54 +0200

    tmp

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
-.PHONY: up down produce
+.PHONY: up down produce lint build
+
+lint:
+	docker run -ti --rm -v $(CURDIR):/apps alpine/flake8:3.5.0 listener/listener.py
+	docker run --rm -v $(CURDIR):/data cytopia/pylint listener/listener.py
 
 run:
 	docker compose up --build
--- listener/listener.py
+++ listener/listener.py
@@ -1,7 +1,6 @@
 from http import HTTPStatus
 import json
 import logging
-import time
 from flask import Flask, request, abort
 from jsonschema import validate, ValidationError
 from pymongo import MongoClient
@@ -10,19 +9,21 @@ SCHEMA_PATH = 'schema.json'
 
 CONNECTION_STRING = "mongodb://root:example@mongo"
 
+
 def get_database():
-   client = MongoClient(CONNECTION_STRING)
-   database = client['webhook']
-   collection = database['events']
-   app.logger.info("dbthings: %s", str(client))
-   app.logger.info("databases: %s", client.list_database_names())
-   items = collection.find()
-   for item in items:
-       app.logger.info("item: %s", item)
+    client = MongoClient(CONNECTION_STRING)
+    database = client['webhook']
+    collection = database['events']
+    app.logger.info("dbthings: %s", str(client))
+    app.logger.info("databases: %s", client.list_database_names())
+    items = collection.find()
+    for item in items:
+        app.logger.info("item: %s", item)
 
-   collection.insert_one({'foo': 'bar'})
+    collection.insert_one({'foo': 'bar'})
 
-   return database
+    return database
+
 
 def data_is_valid(data):
     with open(SCHEMA_PATH) as schema_file:
@@ -34,17 +35,20 @@ def data_is_valid(data):
     except ValidationError:
         return False
 
+
 app = Flask(__name__)
 app.logger.setLevel(logging.INFO)
 
 get_database()
 
+
 @app.get('/')
 def get():
-    dbname = get_database()
-    #collection_name = dbname["user_1_items"]
+    get_database()
+    # collection_name = dbname["user_1_items"]
 
     return "hello!"
+
 
 @app.post('/')
 def webhook_post():
